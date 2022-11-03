3b3ae4a8e9d118a598b9ce6a3c4a87565cd368de
PNdunguMaina <ndunguwamaina@gmail.com>
2022-11-03 18:24:18 +0300

    Add custom methods to right models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  after_save :posted_comments_count
+
+  def posted_comments_count
+    post.update_comments_count
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User'
+  after_save :update_post_counter
 
   def five_recent_comments
     comments.order('created_at Desc').limit(5)
