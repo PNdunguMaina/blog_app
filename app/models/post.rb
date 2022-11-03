0875766bfad436836cca03609e86f044716a70da
PNdunguMaina <ndunguwamaina@gmail.com>
2022-11-03 17:23:13 +0300

    Use models to insert data & add custom methods to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,17 @@ class Post < ApplicationRecord
     has_many :comments
     has_many :likes
     belongs_to :author, class_name: 'User'
+
+    def five_recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
+
+    private
+    def update_comments_count
+        update(CommentsCounter: comments.count)
+    end
+
+    def update_likes_count
+        update(LikesCounter: likes.count)
+    end
 end
