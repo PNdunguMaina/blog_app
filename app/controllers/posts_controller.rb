22115b422c9458b4401ba0f905c5b8bb474aa65c
PNdunguMaina <ndunguwamaina@gmail.com>
2022-11-10 20:26:41 +0300

    Modify posts_controller.rb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.where(author_id: params[:user_id])
+    @user = User.find(params[:user_id])
   end
 
   def show
-    @post = Post.where(id: params[:id])
+    @post = Post.find(params[:id])
+    @user = @post.author
+    @comments = @post.comments
   end
 end
