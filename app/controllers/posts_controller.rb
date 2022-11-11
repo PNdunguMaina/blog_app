9f4305e182c298bc42ad486562c8edb4d4ea117b
PNdunguMaina <ndunguwamaina@gmail.com>
2022-11-11 17:34:01 +0300

    Add create action for Post records

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,30 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create_post
+    # new object from params
+    @post = Post.create(post_params)
+    @first_user = current_user
+    @post.author_id = @first_user.id
+
+    # respond_to block
+    respond_to do |f|
+      f.html do
+        if @post.save
+          flash[:success] = 'Post created successfully!'
+          redirect_to "/users/#{@first_user.id}/posts/"
+        else
+          flash.now[:error] = 'Error creating post!'
+          render :new
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:Title, :Text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/posts/new', to: 'posts#new'
+  post '/posts/new', to: 'posts#create', as: 'create_post'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
