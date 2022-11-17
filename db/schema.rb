f6e8d0df90f331b3e6d064361edf9427ecdfc176
PNdunguMaina <ndunguwamaina@gmail.com>
2022-11-17 15:00:33 +0300

    Add devise gem & set up authentication environment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_132047) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_073730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_132047) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["address"], name: "index_users_on_address"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
